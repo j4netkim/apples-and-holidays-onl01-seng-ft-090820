7f461a144fa11ec1015c6b6220fc240cb61c7e3e
J K <kj688895@gmail.com>
2020-09-20 00:08:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,8 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
-    puts season.to_s.capitalize! << ":"
+    puts season.to_s.capitalize!
+    << ":"
     holiday.each do |holiday, supplies|
       supplies = supplies.join(",")
       holiday = holiday.to_s.split
