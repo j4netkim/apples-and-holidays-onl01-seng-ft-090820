1c63db24753e5261f929ae7a7f401b7a651572e3
J K <kj688895@gmail.com>
2020-09-20 00:01:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,6 +51,8 @@ def all_supplies_in_holidays(holiday_hash)
   end
 end
 
+all_supplies(holiday_supplies)
+
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holiday|
     holiday.collect do |holiday, supply|
